07c0ea53dc15d9c2094acf65797d18709dfda9c2
Sachin Singh V <sachinkumarsingh.bng@gmail.com>
2026-08-18 23:11:22 +0530

    day1 commit1

---
+++ employee.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+department = input("Enter your department: ")
+salary = float(input("Enter your salary: "))
+
+print("\nEmployee Details:")
+print(f"Name: {name}")
+print(f"Age: {age}")
+print(f"Department: {department}")
+print(f"Salary: {salary}")
